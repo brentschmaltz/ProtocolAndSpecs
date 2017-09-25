2fa92b548eca254a3b93bf05a96016b254fd3f64
brentschmaltz <brentschmaltz@hotmail.com>
2017-09-25 09:17:03 -0700

    prototyping tokenbinding

---
+++ TokenBinding/TokenBinding/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace TokenBinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class TokenBindingHandler
+    {
+        public SecurityStatus GenerateBinding(string KeyType, string target, TokenBindingType tokenBindingType, byte[] uniqueData, byte[] extensionData, out TokenBinding tokenBinding)
+        {
+            tokenBinding = null;
+            return SecurityStatus.Success;
+        }
+    }
+
+    public enum SecurityStatus
+    {
+        Success,
+        Failure
+    }
+
+    public enum TokenBindingType
+    {
+        Provided,
+        Referred
+    }
+}
